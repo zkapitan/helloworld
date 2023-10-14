19c6ac50fee9c0db4aab9890048a1998076a59aa
zkapitan <146438732+zkapitan@users.noreply.github.com>
2023-10-14 12:53:25 +0800

    Create helloword.js

---
+++ helloword.js
@@ -0,0 +1,7 @@
+echo "# helloworld" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/zkapitan/helloworld.git
+git push -u origin main
